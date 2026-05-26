c513ae6de9ec2732059202cb00bffedd94aec36d
tangchun <tangchun@OA-S141-14>
2026-05-26 16:10:44 -0400

    This is one of the week 4 in class exercise

---
+++ Assets/Script/Training.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections.Generic;
+
+public class Training : MonoBehaviour
+{
+    public SpriteRenderer piplup;
+    public Camera gameCamera;
+    public Color catchColor;
+    public List<SpriteRenderer> uncaughtCreatures;
+    public List<SpriteRenderer> caughtCreatures;
+    
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isClicked = Mouse.current.leftButton.wasPressedThisFrame;
+        Vector3 mousePosition = Mouse.current.position.ReadValue();
+        Vector3 worldMousePosition = gameCamera.ScreenToWorldPoint(mousePosition);
+        worldMousePosition.z = 0f;
+
+        if (isClicked)
+        {
+            //caughtCreatures
+            //for each element I want to ouput that to the console
+            for (int i = 0; i < uncaughtCreatures.Count; i++)
+            {
+                Debug.Log(caughtCreatures[i]);
+
+            }
+
+            if(caughtCreatures.Count > 0)
+            {
+                Debug.Log(caughtCreatures[0]);
+
+            }
+            
+        }
+        
+
+        if (isClicked && piplup.bounds.Contains(worldMousePosition))
+        {
+            piplup.color = catchColor;
+
+            bool isCreatureCaught = caughtCreatures.Contains(piplup);
+            Debug.Log("Is creature caught[" + isCreatureCaught.ToString() + "]");
+
+            if (!isCreatureCaught)
+            {
+                caughtCreatures.Add(piplup);
+                
+
+            }
+            
+
+            uncaughtCreatures.Remove(piplup);
+            
+
+            
+        }
+
+    }
+}
